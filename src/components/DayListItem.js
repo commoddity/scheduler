228a06ba906f825ff263ed9082f4555a42aad105
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-01-20 15:38:56 -0800

    Added DayListItem componenet to project.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,30 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+
 const classNames = require('classnames');
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  })
+
+  const formatSpots = (props) => 
+    props.spots === 0 
+      ? 'no spots remaining' 
+      : props.spots === 1
+      ? `${props.spots} spot remaining`
+      : `${props.spots} spots remaining`;
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">${props.name}</h2> 
-      <h3 className="text--light">${props.spots}</h3>
+    <li 
+      onClick={() => props.setDay(props.name)}
+      className={dayClass}
+    >
+      <h2>{props.name}</h2> 
+      <h3>{formatSpots(props)}</h3>
     </li>
   );
+  
 }
