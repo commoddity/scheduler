fc696e3b5c145349c0ca88199e9706c9c4c213ff
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-01-21 21:34:35 -0800

    Finished 'Retrieving Appointments' assignment

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Axios from "axios";
 
 import "components/Application.scss";
 
@@ -7,61 +7,7 @@ import "components/Appointment";
 
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Dwayne \"The Rock\" Johnson",
-      interviewer: {
-        id: 1,
-        name: "Shigeru Miyamoto",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Bernie Sanders",
-      interviewer: {
-        id: 1,
-        name: "Sally Struthers",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "George Costanza",
-      interviewer: {
-        id: 1,
-        name: "Abraham Lincoln",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "3pm",
-    interview: {
-      student: "Homer Simpson",
-      interviewer: {
-        id: 1,
-        name: "Gomer Pyle",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
+import getAppointmentsForDay from "helpers/selectors"
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -72,19 +18,23 @@ export default function Application(props) {
 
   const setDay = day => setState({ ...state, day });
 
-  const setDays = days => setState(prev => ({ ...prev, days }));
-
   useEffect(() => {
     async function getDays() {
       try {
-        const response = await axios.get(`/api/days`);
-        setDays(response.data);
+        const [days, appointments] = await Promise.all([
+          Axios.get(`/api/days`).then(res => res.data),
+          Axios.get(`/api/appointments`).then(res => res.data)
+        ])
+        console.log([days, appointments]);
+        setState(prev => ({ ...prev, days, appointments }))
       } catch (error) {
         console.log(error);
       }
     }
     getDays();
   }, []);
+
+  const appointments = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
